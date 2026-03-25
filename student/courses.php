3af9e90f3255d5c0ccdf51b37939b0373b9196b9
govinda <govindarana707@gmail.com>
2026-03-25 11:15:33 +0545

    fix(courses): Add missing style tag wrapper for inline CSS

--- student/courses.php
+++ student/courses.php
@@ -103,7 +103,7 @@ $conn->close();
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link href="../assets/css/style.css" rel="stylesheet">
     <link href="css/student-theme.css" rel="stylesheet">
-
+    <style>
         /* Enhanced Sidebar */
         .sidebar-modern {
             background: white;
